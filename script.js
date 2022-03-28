d492e106143979fa4351c2f325d35cdf2fe1d257
naiadidub <antonio.zaghi1405@gmail.com>
2022-03-29 00:22:41 +0200

    pessimo layout

--- script.js
+++ script.js
@@ -8,22 +8,25 @@ for (let i = 1; i <= 100; i++){
         let pippo = document.createElement("div")
         pippo.append("FizzBuzz")
         document.getElementById('container').append(pippo)
-        pippo.classList.add('fizz-buzz')
+        pippo.classList.add('fizz-buzz', 'col-lg', 'w-25')
     }else if ((i % 3) == 0){
         console.log('Fizz')
         let pluto = document.createElement("div")
         pluto.append("Fizz")
         document.getElementById('container').append(pluto)
+        pluto.classList.add('fizz' , 'col-lg')
     } else if ((i % 5) == 0) {
         console.log('Buzz')
         let giupiter = document.createElement("div")
         giupiter.append("Buzz")
         document.getElementById('container').append(giupiter)
+        giupiter.classList.add('buzz', 'col-lg')
     } else {
         console.log(i)
         let uran = document.createElement("div")
         uran.append(i)
         document.getElementById('container').append(uran)
+        uran.classList.add('norm', 'col-lg')
     }
 }
     
